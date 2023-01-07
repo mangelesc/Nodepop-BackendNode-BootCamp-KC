f8f4e55dc63b1ff3fa5403a4e287f44ad43fd0ec
mangelesc <103500921+mangelesc@users.noreply.github.com>
2023-01-07 18:16:49 +0100

    Mensaje de error personalizado

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+// Ruta de nuestra API
 app.use('/api/ads', require('./routes/api/ads'));
 
 // catch 404 and forward to error handler
@@ -30,12 +32,21 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  
+  res.status(err.status || 500);
+
+  // si es una petición al API, respondemos con formato JSON
+  if (req.originalUrl.startsWith('/api/')) {
+    res.json({ error: err.message });
+    return;
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  // res.status(err.status || 500);
   res.render('error');
 });
 
